e06edc39c4bf69dffe39adc416acd339280b98d4
wwwouaiebe <github.538f6722-9860-473d-9057-57f9bf96554c@guyette.be>
2024-07-27 14:19:50 +0200

    Update dependencies

--- Gruntfile.cjs
+++ Gruntfile.cjs
@@ -29,7 +29,7 @@ module.exports = function ( grunt ) {
 		eslint : {
 			options : {
 				fix : true,
-				overrideConfigFile : '.eslintrc.json'
+				overrideConfigFile : 'eslint.config.js'
 			},
 			target : [ 'src/*.js' ]
 		}
